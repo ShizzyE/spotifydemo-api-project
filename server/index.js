ead858b316337e0cd04cbf12f8202e6c033a6aaa
Shawn Ellis <seellis1@student.fullsail.edu>
2025-10-15 14:23:34 -0400

    testing server

--- server/index.js
+++ server/index.js
@@ -27,5 +27,5 @@ app.use('/favorite_tracks', routers.favorite_track);
 
 
 app.listen(process.env.PORT || 3001, () => {
-    console.log(`Server is running on port ${PORT}`)
+    console.log(`Server is running on port ${process.env.PORT}`)
 });
